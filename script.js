5983e02a9208c7b85d6340af9d680ff5f57443ed
crisvarela304 <soncris4338j@gmail.com>
2025-05-07 17:53:23 -0400

    formulario

---
+++ script.js
@@ -0,0 +1,33 @@
+const students=[];
+
+document.getElementById("studentform").addEventListener("submit",function(e){
+    e.preventDefault();
+
+    const name=document.getElementById("name").value.trim();
+    const lastName=document.getElementById("lastName").value.trim();
+    const grade=parseFloat(document.getElementById("grade").value);
+
+    if(! name || !lastName || isNaN(grade) || grade <1 || grade>7){
+        alert("Error al ingresas los datos")
+    return
+}
+
+    const student={name,lastName,grade}
+    students.push(student)
+    console.log(students)
+    
+    this.reset();
+
+});
+
+const tablebody=document.querySelector("#studentTable tbody");
+function addStudentToTable(student){
+    const row=document.createElement("tr");
+    row.innerHTML=`
+    <td>${student.name}</td>
+        <td>${student.LastName}</td>
+            <td>${student.grade}</td>
+    `;
+tablebody.appendChild(row)
+
+}
