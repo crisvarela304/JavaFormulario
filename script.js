a421f1c81e7b4129a9e572537b4c6e00852d5e0e
crisvarela304 <soncris4338j@gmail.com>
2025-05-14 17:19:24 -0400

    Calculo de promedios

--- script.js
+++ script.js
@@ -30,3 +30,17 @@ function addStudentToTable(student){
 tablebody.appendChild(row)
 
 }
+
+const promedios= document.getElementById("average")
+
+function calcularPromedio(){
+    if(students.length===0){
+        promedios.textContent="Promedio General del Curso : N/A"
+        return
+
+
+    }
+    const total=students.reduce((sum,student)=>sum+student.grade,0);
+    const prom=total/students.length;
+    promedios.textContent="Promedio General del Curso: "+prom.toFixed(2);  
+}
